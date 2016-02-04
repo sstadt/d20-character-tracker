ce8b3fe3300f91f345062f270ed394aedb931ee1
Scott Stadt <scott.stadt@gmail.com>
2016-02-03 18:04:32 -0600

    Added placeholder values to the Character class

--- assets/js/spec/classes/Character.Spec.js
+++ assets/js/spec/classes/Character.Spec.js
@@ -19,8 +19,16 @@ define(['class/Character'], function (Character) {
         expect(defaultChar.name).toEqual('Bob');
       });
 
+      it('should set a default background', function () {
+        expect(defaultChar.background).toEqual('');
+      });
+
       it('should set a default career', function () {
-        expect(defaultChar.career).toEqual('Noob');
+        expect(defaultChar.career).toEqual('Jedi');
+      });
+
+      it('should set an empty specialization list', function () {
+        expect(defaultChar.specializations).toEqual([]);
       });
 
       it('should set a default brawn', function () {
@@ -45,6 +53,30 @@ define(['class/Character'], function (Character) {
 
       it('should set a default presence', function () {
         expect(defaultChar.presence).toEqual(2);
+      });
+
+      it('should set a default earned experience', function () {
+        expect(defaultChar.experience.earned).toEqual(0);
+      });
+
+      it('should set a default spent experience', function () {
+        expect(defaultChar.experience.spent).toEqual(0);
+      });
+
+      it('should set a default amount of credits', function () {
+        expect(defaultChar.wallet.credits).toEqual(500);
+      });
+
+      it('should set an ampty pool of skill points to spend', function () {
+        expect(defaultChar.wallet.skills).toEqual([]);
+      });
+
+      it('should set a default skills object', function () {
+        expect(defaultChar.skills).toEqual(jasmine.any(Object));
+      });
+
+      it('should set a default powers object', function () {
+        expect(defaultChar.powers).toEqual(jasmine.any(Object));
       });
     });
 
