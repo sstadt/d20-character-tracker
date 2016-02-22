26db6bf4548e47e1a13b0018e6e07b028452d057
Scott Stadt <scott.stadt@gmail.com>
2016-02-21 20:04:59 -0600

    spit and polish on the prompts

--- assets/js/lib/components/common/prompt/promptComponent.js
+++ assets/js/lib/components/common/prompt/promptComponent.js
@@ -27,14 +27,18 @@ define([
       label: function (val) {
         if (val !== '') {
           this.show = true;
+          this.$children[0].$els.input.focus();
         }
       }
     },
     methods: {
       submit: function () {
-        this.$dispatch(constants.events.prompt.valueSubmitted, { name: this.name, value: this.promptValue });
-        this.show = false;
-        this.promptValue = '';
+        console.log('submit');
+        if (this.promptValue !== '') {
+          this.$dispatch(constants.events.prompt.valueSubmitted, { name: this.name, value: this.promptValue });
+          this.show = false;
+          this.promptValue = '';
+        }
       },
       cancel: function () {
         var self = this;
