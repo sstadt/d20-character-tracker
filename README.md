230d3ca42bda37f1df662d72c36c8d4c6066e108
Scott Stadt <scott.stadt@gmail.com>
2016-02-26 17:30:42 -0600

    added coverage for diceRoller component

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ a [Sails](http://sailsjs.org) application
 
  - Set up symlink script and store pre-commit hook in the repo: http://stackoverflow.com/questions/3462955/putting-git-hooks-into-repository/3464399#3464399
 
+### Dev to do:
+
+ - move all socket calls to service files so the components can be better isolated for unit testing
+
 ### Setup
 
 Copy the following to config/local.js and fill in values:
--- assets/js/lib/components/diceRoller/diceRoller.Spec.js
+++ assets/js/lib/components/diceRoller/diceRoller.Spec.js
@@ -19,6 +19,75 @@ define([
       expect(component.template).toEqual(jasmine.any(String));
     });
 
+    describe('props', function () {
+      describe('chatHandle', function () {
+        it('should exist', function () {
+          expect(component.props.chatHandle).toEqual(jasmine.any(Object));
+        });
+
+        it('should be a string', function () {
+          expect(component.props.chatHandle.type).toEqual(String);
+        });
+
+        it('should be required', function () {
+          expect(component.props.chatHandle.required).toEqual(true);
+        });
+      });
+    });
+
+    describe('data', function () {
+      var data;
+
+      beforeEach(function () {
+        data = component.data();
+      });
+
+      it('should have a description attribute with an empty string value', function () {
+        expect(data.description).toEqual('');
+      });
+
+      it('should have an ability attribute with a value of 0', function () {
+        expect(data.ability).toEqual(0);
+      });
+
+      it('should have a proficiency attribute with a value of 0', function () {
+        expect(data.proficiency).toEqual(0);
+      });
+
+      it('should have a difficulty attribute with a value of 0', function () {
+        expect(data.difficulty).toEqual(0);
+      });
+
+      it('should have a challenge attribute with a value of 0', function () {
+        expect(data.challenge).toEqual(0);
+      });
+
+      it('should have a boost attribute with a value of 0', function () {
+        expect(data.boost).toEqual(0);
+      });
+
+      it('should have a setback attribute with a value of 0', function () {
+        expect(data.setback).toEqual(0);
+      });
+
+      it('should have a force attribute with a value of 0', function () {
+        expect(data.force).toEqual(0);
+      });
+
+      it('should have a channel attribute that is an object with a label attribute', function () {
+        expect(data.channel).toEqual(jasmine.any(Object));
+        expect(data.channel.label).toEqual(jasmine.any(String));
+      });
+
+      it('should have a localRolls attribute that is an empty array', function () {
+        expect(data.localRolls).toEqual([]);
+      });
+
+      it('should have a channelRolls attribute that is an empty array', function () {
+        expect(data.channelRolls).toEqual([]);
+      });
+    });
+
     describe('methods', function () {
       var componentInstance;
 
@@ -26,11 +95,11 @@ define([
         componentInstance = new Vue(component);
       });
 
-      // describe('#sayHi', function () {
-      //   it('should be a function', function () {
-      //     expect(typeof componentInstance.sayHi).toBe('function');
-      //   });
-      // });
+      describe('#roll', function () {
+        it('should be a function', function () {
+          expect(typeof componentInstance.roll).toBe('function');
+        });
+      });
     });
 
   });
