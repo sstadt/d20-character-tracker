d3aa13ee7fc16ffe9c3aea8ea72e73a918431079
Scott Stadt <scott.stadt@gmail.com>
2017-02-02 16:08:49 -0600

    notifications coverage

--- assets/js/components/notification/notification.Spec.js
+++ assets/js/components/notification/notification.Spec.js
@@ -18,18 +18,66 @@ describe('The notification component', function () {
     expect(component.template).toEqual(jasmine.any(String));
   });
 
-  // describe('methods', function () {
-  //   var componentInstance;
-  //
-  //   beforeEach(function () {
-  //     componentInstance = new Vue(component);
-  //   });
-  //
-  //   describe('#sayHi', function () {
-  //     it('should be a function', function () {
-  //       expect(typeof componentInstance.sayHi).toBe('function');
-  //     });
-  //   });
-  // });
+  describe('methods', function () {
+    var componentInstance;
+
+    beforeEach(function () {
+      componentInstance = new Vue(component);
+    });
+
+    describe('#close', function () {
+      it('should clear all messages', function () {
+        componentInstance.messages = ['foo'];
+        componentInstance.close();
+        expect(componentInstance.messages).toEqual([]);
+      });
+    });
+
+    describe('#addMessage', function () {
+      var timeout = 0;
+
+      beforeEach(function () {
+        componentInstance.addMessage('', 'foo', timeout);
+      });
+
+      it('should add a message to messages', function () {
+        expect(componentInstance.messages.length).toEqual(1);
+      });
+
+      it('should clear the errors after the timeout', function () {
+        setTimeout(function () {
+          expect(componentInstance.messages.length).toEqual(0);
+        }, timeout);
+      });
+    });
+
+    describe('#message', function () {
+      var timeout = 0;
+
+      beforeEach(function () {
+        spyOn(componentInstance, 'addMessage');
+      });
+
+      it('should call the addMessage function with the provided params and an empty type', function () {
+        componentInstance.message('foo', timeout);
+        expect(componentInstance.addMessage).toHaveBeenCalledWith('', 'foo', timeout);
+      });
+
+      it('should call the addMessage function with the provided params and an success type', function () {
+        componentInstance.success('foo', timeout);
+        expect(componentInstance.addMessage).toHaveBeenCalledWith('success', 'foo', timeout);
+      });
+
+      it('should call the addMessage function with the provided params and an warning type', function () {
+        componentInstance.warning('foo', timeout);
+        expect(componentInstance.addMessage).toHaveBeenCalledWith('warning', 'foo', timeout);
+      });
+
+      it('should call the addMessage function with the provided params and an alert type', function () {
+        componentInstance.alert('foo', timeout);
+        expect(componentInstance.addMessage).toHaveBeenCalledWith('alert', 'foo', timeout);
+      });
+    });
+  });
 
 });
