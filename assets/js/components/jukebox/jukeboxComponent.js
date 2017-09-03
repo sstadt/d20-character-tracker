8aebb144da3f455fa2c47c1bc1fb1b5479f767a4
Scott Stadt <scott.stadt@gmail.com>
2017-09-03 15:37:10 -0400

    added volume controls

--- assets/js/components/jukebox/jukeboxComponent.js
+++ assets/js/components/jukebox/jukeboxComponent.js
@@ -7,7 +7,50 @@ module.exports = {
       required: true
     }
   },
+  data() {
+    return {
+      volumeControl: 50,
+      muted: false
+    };
+  },
+  watch: {
+    volumeControl() {
+      this.setVolume();
+    }
+  },
+  computed: {
+    soundStatusIcon() {
+      var iconName;
+
+      if (this.muted) {
+        iconName = 'volume-mute';
+      } else if (this.volumeControl < 34) {
+        iconName = 'volume-low';
+      } else if (this.volumeControl < 67) {
+        iconName = 'volume-mid';
+      } else {
+        iconName = 'volume-high';
+      }
+
+      return iconName;
+    }
+  },
+  mounted() {
+    this.setVolume();
+  },
   methods: {
+    setVolume() {
+      var newVolume = this.volumeControl / 100;
+
+      for (var track in this.$refs) {
+        if (this.muted) {
+          this.$refs[track][0].muted = true;
+        } else {
+          this.$refs[track][0].muted = false;
+          this.$refs[track][0].volume = newVolume;
+        }
+      }
+    },
     playTrack(name) {
       this.$refs[name][0].play();
     },
@@ -20,6 +63,10 @@ module.exports = {
     },
     trackFinished(name) {
       this.$emit('track-finished', name);
+    },
+    toggleMute() {
+      this.muted = !this.muted;
+      this.setVolume();
     }
   }
 };
